a2395c0d829a6e063bec080e097f68797c5175f3
DmitryPotapeyko <potapeiko.ddmitry@yandex.ru>
2018-05-06 23:31:45 +0700

    Share Добавил возможность поделиться прямой ссылкой на фото из DetailsActivity

--- app/src/main/java/com/project/dmitry/yandextest/DetailsActivity.java
+++ app/src/main/java/com/project/dmitry/yandextest/DetailsActivity.java
@@ -1,15 +1,20 @@
 package com.project.dmitry.yandextest;
 
+import android.content.Intent;
+
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
@@ -18,6 +23,8 @@ import com.bumptech.glide.request.target.Target;
 public class DetailsActivity extends AppCompatActivity {
     public static final String EXTRA_SPACE_PHOTO = "SpacePhotoActivity.SPACE_PHOTO";
     private ImageView imageView;
+    private ShareActionProvider shareActionProvider;
+    private ImageData imageData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +32,7 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
         imageView = findViewById(R.id.ac_details_image);
 
-        ImageData imageData = getIntent().getParcelableExtra(EXTRA_SPACE_PHOTO);
+        imageData = getIntent().getParcelableExtra(EXTRA_SPACE_PHOTO);
 
         GlideApp.with(this)
                 .asBitmap()
@@ -54,6 +61,29 @@ public class DetailsActivity extends AppCompatActivity {
                     }
                 })
                 .into(imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        if (shareActionProvider != null) {
+            shareActionProvider.setShareIntent(createShareIntent());
+        }
+        return true;
+    }
+
+    private Intent createShareIntent() {
+        //создание интента с прямой ссылкой на фото
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, imageData.getUrl());
+        return shareIntent;
+
     }
 
     @Override
